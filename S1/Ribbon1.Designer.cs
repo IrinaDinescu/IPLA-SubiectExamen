6a99e40ce1d99a658a66be123051763b48a2cf63
IrinaDinescu <62563909+IrinaDinescu@users.noreply.github.com>
2022-02-03 09:43:32 +0200

    Add files via upload

---
+++ S1/Ribbon1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SubiectExamen
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.btn_B = this.Factory.CreateRibbonButton();
+            this.btn_A = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.btn_A);
+            this.group1.Items.Add(this.btn_B);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // btn_B
+            // 
+            this.btn_B.Label = "B";
+            this.btn_B.Name = "btn_B";
+            this.btn_B.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btn_B_Click);
+            // 
+            // btn_A
+            // 
+            this.btn_A.Label = "A";
+            this.btn_A.Name = "btn_A";
+            this.btn_A.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btn_A_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btn_B;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btn_A;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
